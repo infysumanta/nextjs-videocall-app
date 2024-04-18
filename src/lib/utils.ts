bf1982dfd700b33b9c1f6cce52679c378a4144d1
Sumanta <sumantablog@gmail.com>
2024-04-18 11:35:00 +0530

    Refactor imports and update dependencies

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,33 +1,6 @@
-import clsx, { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function getMailToLink(
-  meetingLink: string,
-  startsAt?: Date,
-  description?: string,
-) {
-  const startDateFormatted = startsAt
-    ? startsAt.toLocaleString("en-US", {
-        dateStyle: "full",
-        timeStyle: "short",
-      })
-    : undefined;
-
-  const subject =
-    "Join my meeting" + (startDateFormatted ? ` at ${startDateFormatted}` : "");
-
-  const body =
-    `Join my meeting at ${meetingLink}.` +
-    (startDateFormatted
-      ? `\n\nThe meeting starts at ${startDateFormatted}.`
-      : "") +
-    (description ? `\n\nDescription: ${description}` : "");
-
-  return `mailto:?subject=${encodeURIComponent(
-    subject,
-  )}&body=${encodeURIComponent(body)}`;
+  return twMerge(clsx(inputs))
 }
